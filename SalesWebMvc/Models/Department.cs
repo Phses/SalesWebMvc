e46314c1ebf7515d245b6e72939afcd9f2ceaf47
Phses <phsouzaesilva@gmail.com>
2023-03-27 07:01:00 -0300

    Adicionando outras entidades do modelo e nova migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -4,5 +4,17 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public void addSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.totalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,31 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public List<SalesRecord> SalesRecords { get; set; }
+
+        public void addSales(SalesRecord sale)
+        {
+            SalesRecords.Add(sale);
+        }
+
+        public void removeSales(SalesRecord sale)
+        {
+            SalesRecords.Remove(sale);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
