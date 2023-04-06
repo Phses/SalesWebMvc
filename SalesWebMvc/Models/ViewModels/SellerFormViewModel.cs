a7fbe2d43edcc4a678f7fa0ed0534362e606e2ea
Phses <phsouzaesilva@gmail.com>
2023-04-06 06:30:46 -0300

    add department to the form and create viewModel

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+    }
+}
