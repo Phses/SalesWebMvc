c46694351247d8ea6a4fe2034bb38b1d061e472f
Phses <phsouzaesilva@gmail.com>
2023-04-12 06:22:25 -0300

    validacao

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -5,15 +5,23 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(100.0, 50000.0, ErrorMessage = "O valor precisa estar entre {1} e {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
